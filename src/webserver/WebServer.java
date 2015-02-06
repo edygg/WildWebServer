290e2ae27c0cfa627358523d7edb4547507adb00
Carlos Banegas <cbanegas@unitec.edu>
2015-02-06 15:18:57 -0600

    Web Server Added
    Servidor inicial agregado con implementación de threads.

--- src/webserver/WebServer.java
+++ src/webserver/WebServer.java
@@ -1,10 +1,47 @@
 
 package webserver;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author Edilson Gonzalez
  */
 public class WebServer extends Thread {
+    private final int PORT = 8080; //Puerto en el que se recibiran requests de clientes
+    ServerSocket server = null;
+   
+    public WebServer() throws Exception {
+        server = new ServerSocket(PORT);
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try {
+                Socket clientRequest = server.accept();
+                PrintWriter out = new PrintWriter(clientRequest.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientRequest.getInputStream()));
+                String data = in.readLine();
+                
+                while (data != null) {
+                    System.out.println(data);
+                    data = in.readLine();
+                }
+                
+                in.close();
+                out.close();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
     
+    
+   
+   
 }

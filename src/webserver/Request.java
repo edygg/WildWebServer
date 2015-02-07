f59ca8dfe400b0aad3ee98dcfbdb23a3a8345a5b
Edilson Gonzalez <edilson.gonza@unitec.edu>
2015-02-07 12:03:53 -0600

    Adding states to Request

--- src/webserver/Request.java
+++ src/webserver/Request.java
@@ -2,6 +2,7 @@
 package webserver;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.PrintWriter;
 
 /**
@@ -10,24 +11,33 @@ import java.io.PrintWriter;
  */
 public class Request extends Thread {
     
-    private PrintWriter out;
+    private DataOutputStream out;
     private BufferedReader in;
+    private int state;
+    public static int READY = 1;
+    public static int RUNNING = 2;
+    public static int STOPED = 3;
 
-    public Request(PrintWriter out, BufferedReader in) {
+    public Request(DataOutputStream out, BufferedReader in) {
         this.out = out;
         this.in = in;
+        this.state = READY; 
     }
 
     @Override
     public void run() {
-        
+        this.state = RUNNING;
+    }
+    
+    public int getRequestState() {
+        return state;
     }
     
     public BufferedReader getIn() {
         return in;
     }
 
-    public PrintWriter getOut() {
+    public DataOutputStream getOut() {
         return out;
     }
 
